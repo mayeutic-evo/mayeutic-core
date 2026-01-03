ef1913a585bcc40aefa32981bb99ef78e0eeea60
mayeutic-evo <mayeutic.evo@gmail.com>
2026-01-03 14:34:02 -0300

    feat: add processEvent skeleton for Nukleo

---
+++ engine/process_event.ts
@@ -0,0 +1,7 @@
+// process_event.ts
+export function processEvent(input: unknown) {
+    // TODO: map input to context
+    // TODO: evaluate Core tables
+    // TODO: return recommended action
+  }
+  
